a6f91f97778d44b804f522fffa753c5b38c61888
Ivan <ivan.salazarv@gmail.com>
2018-03-26 21:07:41 -0600

    Commenting out initial code. Adding grid in state and drawing it.

--- src/qwircl/core.cljs
+++ src/qwircl/core.cljs
@@ -2,40 +2,54 @@
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]))
 
+(def width 500)
+(def height 500)
+(def tiles 25)
+(def size 20)
+
 (defn setup []
   ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
   ; Set color mode to HSB (HSV) instead of default RGB.
   (q/color-mode :hsb)
   ; setup function returns initial state. It contains
-  ; circle color and position.
-  {:color 0
-   :angle 0})
+  ; the empty grid of tiles
+  (vec (repeat tiles (vec (repeat tiles nil)))))
 
 (defn update-state [state]
   ; Update sketch state by changing circle color and position.
-  {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.1)})
+  ;; {:color (mod (+ (:color state) 0.7) 255)
+  ;;  :angle (+ (:angle state) 0.1)}
+  state)
 
 (defn click-event [state event]
   ; Hacky demo of updating state based on click coordinates.
-  {:color (mod (:x event) 255)
-   :angle (:y event)})
+  ;; {:color (mod (:x event) 255)
+  ;;  :angle (:y event)}
+  state)
+
+;; (defn draw-state [state]
+;;   ; Clear the sketch by filling it with light-grey color.
+;;   (q/background 240)
+;;   ; Set circle color.
+;;   (q/fill (:color state) 255 255)
+;;   ; Calculate x and y coordinates of the circle.
+;;   (let [angle (:angle state)
+;;         x (* 150 (q/cos angle))
+;;         y (* 150 (q/sin angle))]
+;;     ; Move origin point to the center of the sketch.
+;;     (q/with-translation [(/ (q/width) 2)
+;;                          (/ (q/height) 2)]
+;;       ; Draw the circle.
+;;       (q/ellipse x y 100 100))))
 
 (defn draw-state [state]
-  ; Clear the sketch by filling it with light-grey color.
   (q/background 240)
-  ; Set circle color.
-  (q/fill (:color state) 255 255)
-  ; Calculate x and y coordinates of the circle.
-  (let [angle (:angle state)
-        x (* 150 (q/cos angle))
-        y (* 150 (q/sin angle))]
-    ; Move origin point to the center of the sketch.
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
-      ; Draw the circle.
-      (q/ellipse x y 100 100))))
+  (q/fill 0 255 255)
+  (q/stroke 0 0 0)
+  (doseq [x (range tiles) 
+          y (range tiles)]
+    (q/rect (* size x) (* size y) size size)))
 
 ; hacky way of replacing the default cljs.main page with a canvas for quil
 (def ^:const canvas-id "sketch")
@@ -47,7 +61,7 @@
 ; start quil
 (q/defsketch hello-world
     :host canvas-id
-    :size [500 500]
+    :size [width height]
     ; setup function called only once, during sketch initialization.
     :setup setup
     ; update-state is called on each iteration before draw-state.
